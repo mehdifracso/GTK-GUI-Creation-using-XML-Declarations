32a07d1e3d680e40a5b5419a5bf87e0c91e6c08d
mehdi fracso <mehdifracso@gmail.com>
2019-03-24 16:47:28 +0100

    Ignores the white space in the XML file

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 
 
 
-
 int main (int argc, char *argv[])
 {
     gtk_init(&argc,&argv);
+
+    //  Pour ignorer les espaces dans le fichier XML
+    xmlKeepBlanksDefault(0);
 
     GtkWidget *mainWindow = parseStructureAndCreateGUI("mainWidgetsStructure.xml");
 
